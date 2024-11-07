1583c9779abd387967af5539204ca8245e35ea75
Wesley Xavier <wesley.xvier@gmail.com>
2024-11-07 10:11:50 -0300

    :books: docs: modified version of malba_than to unuse pow operator.

--- src/malba_tahan.py
+++ src/malba_tahan.py
@@ -1,7 +1,15 @@
+# def malba_tahan():
+#     soma = 0
+#     for i in range (64):
+#         soma += 2**i
+#     return soma
+
 def malba_tahan():
     soma = 0
+    ant = 1
     for i in range (64):
-        soma += 2**i
+        soma += ant
+        ant *= 2
     return soma
 
 print(malba_tahan())
